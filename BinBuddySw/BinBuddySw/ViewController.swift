6a7a2756d2ab89f9e7aff7384a627b00c23bba1c
oyuyump <yuyumpoptec@gmail.com>
2024-12-19 23:49:38 -0600

    Add TabBar with icons test

---
+++ BinBuddySw/BinBuddySw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BinBuddySw
+//
+//  Created by OYuuyuMP on 19/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
