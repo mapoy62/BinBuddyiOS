9b1da281c4d441ef529eb357aec0f30212a93ac7
oyuyump <yuyumpoptec@gmail.com>
2024-12-22 23:14:08 -0600

    WIP TweetCollection

---
+++ BinBuddySw/BinBuddySw/Controller/explore/TweetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TweetTableViewCell.swift
+//  BinBuddySw
+//
+//  Created by OYuuyuMP on 22/12/24.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
